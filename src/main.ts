f6baf1b418614576613bea59b9024f312a67f79e
vraj4503 <129033176+vraj4503@users.noreply.github.com>
2025-07-01 18:15:43 +0530

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -8,26 +8,15 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: (
-      origin: string | undefined,
-      callback: (err: Error | null, allow?: boolean) => void,
-    ) => {
-      const allowedOrigins = [
-        'http://localhost:3000',
-        'https://hrms-frontend-git-main-vrajs-projects-c97b9bd7.vercel.app',
-        'https://hrms-frontend-beryl.vercel.app',
-        'https://hrms-frontend-beryl.vercel.app'
-      ];
-      if (!origin || allowedOrigins.includes(origin)) {
-        callback(null, true);
-      } else {
-        callback(new Error('Not allowed by CORS'));
-      }
-    },
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
-    allowedHeaders: ['Content-Type', 'Authorization'],
-    credentials: true,
-  });
+  origin: [
+    'http://localhost:3000',
+    'https://hrms-frontend-git-main-vrajs-projects-c97b9bd7.vercel.app',
+    'https://hrms-frontend-beryl.vercel.app',
+  ],
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+  credentials: true,
+});
 
   app.useGlobalPipes(new ValidationPipe());
 
